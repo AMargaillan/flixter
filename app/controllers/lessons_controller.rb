f8c8e1007437cc7841c2da3c01343e559845f023
AMargaillan <adeline.margaillan@gmail.com>
2019-07-21 17:03:23 +0000

    enrolled in

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,13 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+  
+  def require_authorized_for_current_lesson
+    if current_user != current_user.enrolled_in?
+      redirct_to course_path, alert: 'You must be enrolled to view lessons'
+    end
+  end
+
   def show
   end
 
@@ -8,4 +17,6 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+
 end
