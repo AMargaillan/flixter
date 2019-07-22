b659021b5d8f991aa97bca7e6c19c14e0f4d24a4
AMargaillan <adeline.margaillan@gmail.com>
2019-07-22 00:56:54 +0000

    payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,8 +3,8 @@ class LessonsController < ApplicationController
   before_action :require_authorized_for_current_lesson
   
   def require_authorized_for_current_lesson
-    if current_user != current_user.enrolled_in?
-      redirct_to course_path, alert: 'You must be enrolled to view lessons'
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to courses_path, alert: 'You must be enrolled to view lessons'
     end
   end
 
